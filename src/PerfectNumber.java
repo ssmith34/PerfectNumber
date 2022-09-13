6b331b4a564651b40e88d5cfef7275212ad310ea
ssmith34 <110691416+ssmith34@users.noreply.github.com>
2022-09-13 17:35:03 -0400

    Finished Product

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -2,6 +2,20 @@ public class PerfectNumber {
 
     public static boolean isPerfectNumber (int number) {
         // Edge Cases
-        
+        if (number < 1) {
+            return false;
+        }
+
+        // Variable Declaration
+        int sum = 0;
+
+        // iterate through number
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+                System.out.println(sum);
+            }
+        }
+        return sum == number ? true : false;
     }
 }
